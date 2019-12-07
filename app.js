939bd57f5fc1c7fa419ffe3ae579346aba05df6f
AndyDupuis <54333765+AndyDupuis@users.noreply.github.com>
2019-12-06 21:10:15 -0400

    Implemented jquery on form

---
+++ app.js
@@ -0,0 +1,93 @@
+
+
+	function isEmail(email) {
+  		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  		return regex.test(email);
+	}
+
+	$("#submitButton").click(function() {
+	
+		var fieldsMissing = "";
+		var errorMessage = "";
+	
+		if($("#email").val() == ""){
+			fieldsMissing += "<br>Email";
+		}
+		if($("#firstName").val() == ""){
+			fieldsMissing += "<br>First Name";
+		}
+		if($("#lastName").val() == ""){
+			fieldsMissing += "<br>Last Name";
+		}
+		if($("#subject").val() == ""){
+			fieldsMissing += "<br>Subject";
+		} 
+		if($("#message").val() == ""){
+			fieldsMissing += "<br>Message";
+		}
+
+		if (fieldsMissing != ""){
+			errorMessage += "<p>The following field(s) are missing:" + fieldsMissing;
+		}
+
+		if(isEmail($("#email").val()) == false){
+			errorMessage += "<p>Your email address is not valid</p>";
+		}
+
+		if (errorMessage != ""){
+			$("#errorMessage").addClass("alert alert-danger");
+			$("#errorMessage").html(errorMessage);
+		} else {
+			$("#errorMessage").hide();
+			$("#successMessage").addClass("alert alert-success");
+			$("#successMessage").show();
+		}
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
