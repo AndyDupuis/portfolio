ba78e43f4db2262ae9327524ff2f9d8d843e6dd4
AndyDupuis <dupuis@bluelightanalytics.com>
2021-05-24 10:01:20 -0300

    made responsive for larger screen sizes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function isEmail(email) {
 	return regex.test(email);
 }
 
+// input validation for the contact form
 $("#submitButton").click(function() {
 
 
@@ -45,22 +46,42 @@ $("#submitButton").click(function() {
 
 });
 
+// when you expand the bio, show the rest of the text and add bottom margin to the image so that the next section doesn't overlap
 function viewMore() {
 	document.getElementById('viewMore').style.cssText = 'display: none;';
 	document.getElementById('overflowText1').style.cssText = 'display: block;';
 	document.getElementById('overflowText2').style.cssText = 'display: block;';
 	document.getElementById('overflowText3').style.cssText = 'display: block;';
 	document.getElementById('viewLess').style.cssText = 'display: block;';
-	document.getElementById('headshot').style.cssText = 'margin-bottom: 60%;'
+	
+	const mediaQuerySmall = window.matchMedia('(min-width: 768px)');
+	const mediaQueryMedium = window.matchMedia('(min-width: 830px)');
+	const mediaQueryLarge = window.matchMedia('(min-width: 1000px)');
+	const mediaQueryXLarge = window.matchMedia('(min-width: 1300px)');
+
+	if (mediaQueryXLarge.matches) {
+		document.getElementById('headshot').style.cssText = 'margin-bottom: 15%;'
+	}
+	else if (mediaQueryLarge.matches) {
+		document.getElementById('headshot').style.cssText = 'margin-bottom: 38%;'
+	}
+	else if (mediaQueryMedium.matches) {
+		document.getElementById('headshot').style.cssText = 'margin-bottom: 65%;'
+	} 
+	else if (mediaQuerySmall.matches) {
+		document.getElementById('headshot').style.cssText = 'margin-bottom: 80%;'
+	}
+
 }
 
+// when you collapse the bio, stop showing the extra text and remove the added bottom margin from the image
 function viewLess() {
 	document.getElementById('viewLess').style.cssText = 'display: none;';
 	document.getElementById('overflowText1').style.cssText = 'display: none;';
 	document.getElementById('overflowText2').style.cssText = 'display: none;';
 	document.getElementById('overflowText3').style.cssText = 'display: none;';
 	document.getElementById('viewMore').style.cssText = 'display: block;';
-	document.getElementById('headshot').style.cssText = 'margin-bottom: 10%;'
+	document.getElementById('headshot').style.cssText = ''
 }
 
 	
